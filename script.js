f17f470c25d82b0a69865720933bec71a350ff94
Lisa Wand <lisavenneker21@gmail.com>
2021-03-21 19:13:39 -0400

    score board added

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ showRock = (event) => {
   let click = document.querySelector("#show-user-click")
   click.innerHTML = "Rock"
   userChoice = "Rock"
-
   random()
-  // showComp.innerHTML = computerPick
   compare()
 }
 let rockBtn = document.querySelector("#rock-btn")
@@ -17,12 +15,9 @@ showPaper = (event) => {
   let click = document.querySelector("#show-user-click")
   click.innerHTML = "Paper"
   userChoice = "Paper"
-
   random()
-  // showComp.innerHTML = computerPick
   compare()
 }
-
 let paperBtn = document.querySelector("#paper-btn")
 paperBtn.addEventListener("click", showPaper)
 
@@ -31,18 +26,14 @@ showScissors = (event) => {
   let click = document.querySelector("#show-user-click")
   click.innerHTML = "Scissors"
   userChoice = "Scissors"
-
   random()
-  // showComp.innerHTML = computerPick
   compare()
 }
-
 let scissorsBtn = document.querySelector("#scissors-btn")
 scissorsBtn.addEventListener("click", showScissors)
 
 
 // create computer function for R P S - random()
-
 let optionsPC = ["Rock", "Paper", "Scissors"]
 
 random = () => {
@@ -50,10 +41,9 @@ random = () => {
   showComp.innerHTML = computerPick
   console.log("Computer picks", computerPick)
 }
-
 let showComp = document.querySelector("#computer-pick")
 
-// create if or SWITCH to show how you can win - BEGIN GAME
+// Create if or SWITCH to show how you can win - BEGIN GAME
 let showResult = document.querySelector("#result")
 
 compare = () => {
@@ -61,32 +51,29 @@ compare = () => {
     showResult.innerHTML = "Its a tie!"
   } else if (userChoice === "Paper" && showComp.innerHTML === "Rock") {
     showResult.innerHTML = "You won!"
+    player.innerHTML ++
   } else if (userChoice === "Scissors" && showComp.innerHTML === "Rock") {
     showResult.innerHTML = "Loser!"
+    enemy.innerHTML ++
   } else if (userChoice === "Rock" && showComp.innerHTML === "Paper") {
     showResult.innerHTML = "Loser!"
+    enemy.innerHTML ++
   } else if (userChoice === "Rock" && showComp.innerHTML === "Scissors") {
     showResult.innerHTML = "You Won!"
+    player.innerHTML ++
   } else if (userChoice === "Paper" && showComp.innerHTML === "Scissors") {
     showResult.innerHTML = "Loser!"
+    enemy.innerHTML ++
   } else if (userChoice === "Scissors" && showComp.innerHTML === "Paper") {
     showResult.innerHTML = "You Won!"
+    player.innerHTML ++
   }
 }
 
-
-
-// compare = () => {
-//   if (userChoice === "Rock" && computerPick === "Rock") {
-//     showResult.innerHTML = "Its a tie!"
-//     console.log("its a tie")
-//   } else if (userChoice === "Paper" && computerPick === "Rock") {
-//     showResult.innerHTML = "You won!"
-//     console.log("You won")
-//   } else if (userChoice === "Scissors" && computerPick === "Rock") {
-//     showResult.innerHTML = "Loser!"
-//     console.log("loser")
-//   }
-// }
+// Keep Score of Game
+let player = 0
+player = document.querySelector("#player-score")
+let enemy = 0
+enemy = document.querySelector("#enemy-score")
 
 
